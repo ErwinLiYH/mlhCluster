ffb04fedaeb420810ad30b651faa37b1f6b33e3e
ErwinLi <60261171+ErwinLiYH@users.noreply.github.com>
2022-08-10 11:27:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="mlhCluster",
-    version="2.0.0",
+    version="2.0.1",
     author="ErwinLi",
     author_email="1779599839@qq.com",
     description="multi-layer hierarachical clustering",
